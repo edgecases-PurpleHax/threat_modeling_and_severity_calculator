d6ef97e8e063265950ba59d357ed6ed8a22fcca4
Restyled.io <commits@restyled.io>
2022-03-24 21:33:23 +0000

    Restyled by yapf

--- Threat_Modeling/STRIDE.py
+++ Threat_Modeling/STRIDE.py
@@ -3,6 +3,7 @@ import time
 
 
 class Stride:
+
     def __init__(self):
         self.feature_name = input("What feature are you threat modeling?\r\n")
         self.spoofing = self.spoofing_questions()
@@ -21,37 +22,31 @@ class Stride:
         print("IP Spoofing\r\n")
         while ip_spoofing_next.lower()[0] != "n":
             ip_spoofing_question = input(
-                "What opportunities are there to spoof IP addresses?\r\n"
-            )
+                "What opportunities are there to spoof IP addresses?\r\n")
             ip_spoofing_explanation = input(
                 "Describe how this spoofing threat would be accomplished: \r\n"
             )
             spoofing["IP"][ip_spoofing_question] = ip_spoofing_explanation
             ip_spoofing_next = input(
-                "Would you like to enter another IP spoofing threat?\r\n"
-            )
+                "Would you like to enter another IP spoofing threat?\r\n")
         while dns_spoofing_next.lower()[0] != "n":
             dns_spoofing_question = input(
-                "What opportunities are there to spoof DNS?\r\n"
-            )
+                "What opportunities are there to spoof DNS?\r\n")
             dns_spoofing_explanation = input(
                 "Describe how this spoofing threat would be accomplished: \r\n"
             )
             spoofing["DNS"][dns_spoofing_question] = dns_spoofing_explanation
             dns_spoofing_next = input(
-                "Would you like to enter another DNS spoofing threat?\r\n"
-            )
+                "Would you like to enter another DNS spoofing threat?\r\n")
         while arp_spoofing_next.lower()[0] != "n":
             arp_spoofing_question = input(
-                "What opportunities are there to spoof ARP?\r\n"
-            )
+                "What opportunities are there to spoof ARP?\r\n")
             arp_spoofing_explanation = input(
                 "Describe how this spoofing threat would be accomplished: \r\n"
             )
             spoofing["ARP"][arp_spoofing_question] = arp_spoofing_explanation
             arp_spoofing_next = input(
-                "Would you like to enter another ARP spoofing threat?\r\n"
-            )
+                "Would you like to enter another ARP spoofing threat?\r\n")
         return spoofing
 
     @staticmethod
@@ -61,19 +56,16 @@ class Stride:
         request_next = "y"
         while token_next.lower()[0] != "n":
             token_question = input(
-                "What opportunities are there to tamper with tokens?\r\n"
-            )
+                "What opportunities are there to tamper with tokens?\r\n")
             token_explanation = input(
                 "Describe how this tampering threat would be accomplished:\r\n"
             )
             tampering["Token"][token_question] = token_explanation
             token_next = input(
-                "Would you like to enter another Token Tampering Threat?\r\n"
-            )
+                "Would you like to enter another Token Tampering Threat?\r\n")
         while request_next.lower()[0] != "n":
             request_question = input(
-                "What opportunities are there to tamper with requests?\r\n"
-            )
+                "What opportunities are there to tamper with requests?\r\n")
             request_explanation = input(
                 "Describe how this tampering threat would be accomplished:\r\n"
             )
@@ -94,8 +86,7 @@ class Stride:
             )
             repudiation["Logging"][logging_question] = logging_explanation
             logging_next = input(
-                "Would you like to enter another Repudiation threat?\r\n"
-            )
+                "Would you like to enter another Repudiation threat?\r\n")
         return repudiation
 
     @staticmethod
@@ -110,17 +101,13 @@ class Stride:
         backend_info_next = "y"
         while sens_docs_next.lower()[0] != "n":
             sens_docs_question = input(
-                "Are there any Sensitive Documents (Enter each Document? \r\n"
-            )
+                "Are there any Sensitive Documents (Enter each Document? \r\n")
             sens_docs_explanation = input(
-                "Describe how these Documents could be exposed: \r\n"
-            )
+                "Describe how these Documents could be exposed: \r\n")
             information_disclosure["Sensitive_Docs"][
-                sens_docs_question
-            ] = sens_docs_explanation
+                sens_docs_question] = sens_docs_explanation
             sens_docs_next = input(
-                "Would you like to enter another Sensitive Document?\r\n"
-            )
+                "Would you like to enter another Sensitive Document?\r\n")
         while app_info_next.lower()[0] != "n":
             app_info_question = input(
                 "Is there any Sensitive Application information? (Such as Version, etc): \r\n"
@@ -129,11 +116,9 @@ class Stride:
                 "Describe how this information could be used maliciously: \r\n"
             )
             information_disclosure["Application_Info"][
-                app_info_question
-            ] = app_info_explanation
+                app_info_question] = app_info_explanation
             app_info_next = input(
-                "Would you like to enter more Application Information?\r\n"
-            )
+                "Would you like to enter more Application Information?\r\n")
         while backend_info_next.lower()[0] != "n":
             backend_question = input(
                 "Is there any Sensitive Backend information? (Such as Version, etc) \r\n"
@@ -142,11 +127,9 @@ class Stride:
                 "Describe how this information could be used maliciously: \r\n"
             )
             information_disclosure["Backend_Service_Disclosure"][
-                backend_question
-            ] = backend_explanation
+                backend_question] = backend_explanation
             backend_info_next = input(
-                "Would you like to enter more Backend Information?\r\n"
-            )
+                "Would you like to enter more Backend Information?\r\n")
         return information_disclosure
 
     @staticmethod
@@ -159,25 +142,29 @@ class Stride:
             system_dos_question = input(
                 "How can an attacker deny service to the System? (Application, etc)? \r\n"
             )
-            system_dos_explanation = input("How would this be accomplished? \r\n")
+            system_dos_explanation = input(
+                "How would this be accomplished? \r\n")
             dos["System_DOS"][system_dos_question] = system_dos_explanation
             system_dos_next = input(
-                "Would you like to enter more another System DoS?\r\n"
-            )
+                "Would you like to enter more another System DoS?\r\n")
         while user_dos_next.lower()[0] != "n":
             user_dos_question = input(
                 "How can an attacker deny service to the User? (Application, etc)? \r\n"
             )
-            user_dos_explanation = input("How would this be accomplished? \r\n")
+            user_dos_explanation = input(
+                "How would this be accomplished? \r\n")
             dos["User_DOS"][user_dos_question] = user_dos_explanation
-            user_dos_next = input("Would you like to enter another User DoS?\r\n")
+            user_dos_next = input(
+                "Would you like to enter another User DoS?\r\n")
         while network_dos_next.lower()[0] != "n":
             network_dos_question = input(
                 "How can an attacker deny service to the Network? (Application, etc)? \r\n"
             )
-            network_dos_explanation = input("How would this be accomplished? \r\n")
+            network_dos_explanation = input(
+                "How would this be accomplished? \r\n")
             dos["Network_DOS"][network_dos_question] = network_dos_explanation
-            network_dos_next = input("Would you like to enter another Network DoS?\r\n")
+            network_dos_next = input(
+                "Would you like to enter another Network DoS?\r\n")
         return dos
 
     @staticmethod
@@ -198,7 +185,8 @@ class Stride:
             backend_pr_question = input(
                 "How can an attacker Escalate Backend Privileges? (Laterally, etc)? \r\n"
             )
-            backend_pr_explanation = input("How would this be accomplished? \r\n")
+            backend_pr_explanation = input(
+                "How would this be accomplished? \r\n")
             eop["Backend_Privs"][backend_pr_question] = backend_pr_explanation
             backend_pr_next = input(
                 "Would you like to enter another Backend Escalation of Privileges?\r\n"
